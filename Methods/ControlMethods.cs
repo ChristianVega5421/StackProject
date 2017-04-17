0a91f8a97551ca9dd086cb6cc4a522a9812f65a2
Christian Vega <Christian Vega>
2017-04-16 21:12:59 -0500

    Project

---
+++ Methods/ControlMethods.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicle;
+
+
+
+namespace Methods
+{
+    public class ControlMethods
+    {
+
+        public ControlMethods() { }
+
+        /// <summary>
+        /// Method to control if the car can or can not be on the road, with the provided information it has been supposed that the date
+        /// will be in European Format (used in Ecuador) and the time will be the hour of the day the vehicle will be on road so it can not be lesser than 0 or greater than 24
+        /// I am returning a message so i can compare it on the unit tests
+        /// 
+        /// </summary>
+        /// <param name="vehicle"></param>
+        /// <param name="date"></param>
+        /// <param name="hourOfDay"></param>
+        /// <returns>VehicleObject</returns>
+        public VehicleObject VehicleOnRoad(VehicleObject vehicle, string date, TimeSpan hourOfDay) {
+            TimeSpan LowerLimit = new TimeSpan(0,0,0);
+            TimeSpan HigherLimit = new TimeSpan(24, 0, 0);
+            if (TimeSpan.Compare(hourOfDay, LowerLimit) == 1 && TimeSpan.Compare(hourOfDay, HigherLimit) == -1) {
+                DateTime Date;
+                string[] formats = { "dd-MM-yyyy", "dd/MM/yyyy" };
+                if (DateTime.TryParseExact(date, formats, System.Globalization.CultureInfo.InvariantCulture, DateTimeStyles.None, out Date))
+                {
+                    string DayOfTheWeek = Date.ToString("ddd");
+                    TimeSpan BeginingOfMorning = new TimeSpan(7,0,0);
+                    TimeSpan BeginingOfNoon = new TimeSpan(16,0,0);
+                    TimeSpan EndOfMorning = new TimeSpan(9, 30, 0);
+                    TimeSpan EndOfNoon = new TimeSpan(19, 30, 0);
+                    string PlateNumberLastDigit = vehicle.VehLicensePlateNumber.Substring(vehicle.VehLicensePlateNumber.Length - 1);
+                    switch (PlateNumberLastDigit)
+                    {
+                        case "1":
+                        case "2":
+                            if (DayOfTheWeek.Equals("Mon"))
+                            {
+                                if ((TimeSpan.Compare(hourOfDay, BeginingOfMorning) == 1 && TimeSpan.Compare(hourOfDay, EndOfMorning) == -1) ||
+                                        (TimeSpan.Compare(hourOfDay, BeginingOfNoon) == 1 && TimeSpan.Compare(hourOfDay, EndOfNoon) == -1))                               
+                                {
+                                    vehicle.VehMessage = "Car can't be on the road";
+                                    return vehicle;
+                                }
+                                vehicle.VehMessage = "Car can be on the road";
+                                return vehicle;
+                            }
+                            vehicle.VehMessage = "Car can be on the road";
+                            return vehicle;
+                        case "3":
+                        case "4":
+                            if (DayOfTheWeek.Equals("Tue"))
+                            {
+                                if ((TimeSpan.Compare(hourOfDay, BeginingOfMorning) == 1 && TimeSpan.Compare(hourOfDay, EndOfMorning) == -1) ||
+                                        (TimeSpan.Compare(hourOfDay, BeginingOfNoon) == 1 && TimeSpan.Compare(hourOfDay, EndOfNoon) == -1))
+                                {
+                                    vehicle.VehMessage = "Car can't be on the road";
+                                    return vehicle;
+                                }
+                                vehicle.VehMessage = "Car can be on the road";
+                                return vehicle;
+                            }
+                            vehicle.VehMessage = "Car can be on the road";
+                            return vehicle;
+                        case "5":
+                        case "6":
+                            if (DayOfTheWeek.Equals("Wed"))
+                            {
+                                if ((TimeSpan.Compare(hourOfDay, BeginingOfMorning) == 1 && TimeSpan.Compare(hourOfDay, EndOfMorning) == -1) ||
+                                        (TimeSpan.Compare(hourOfDay, BeginingOfNoon) == 1 && TimeSpan.Compare(hourOfDay, EndOfNoon) == -1))
+                                {
+                                    vehicle.VehMessage = "Car can't be on the road";
+                                    return vehicle;
+                                }
+                                vehicle.VehMessage = "Car can be on the road";
+                                return vehicle;
+                            }
+                            vehicle.VehMessage = "Car can be on the road";
+                            return vehicle;
+                        case "7":
+                        case "8":
+                            if (DayOfTheWeek.Equals("Thr"))
+                            {
+                                if ((TimeSpan.Compare(hourOfDay, BeginingOfMorning) == 1 && TimeSpan.Compare(hourOfDay, EndOfMorning) == -1) ||
+                                        (TimeSpan.Compare(hourOfDay, BeginingOfNoon) == 1 && TimeSpan.Compare(hourOfDay, EndOfNoon) == -1))
+                                {
+                                    vehicle.VehMessage = "Car can't be on the road";
+                                    return vehicle;
+                                }
+                                vehicle.VehMessage = "Car can be on the road";
+                                return vehicle;
+                            }
+                            vehicle.VehMessage = "Car can be on the road";
+                            return vehicle;
+                        case "9":
+                        case "0":
+                            if (DayOfTheWeek.Equals("Fri"))
+                            {
+                                if ((TimeSpan.Compare(hourOfDay, BeginingOfMorning) == 1 && TimeSpan.Compare(hourOfDay, EndOfMorning) == -1) ||
+                                        (TimeSpan.Compare(hourOfDay, BeginingOfNoon) == 1 && TimeSpan.Compare(hourOfDay, EndOfNoon) == -1))
+                                {
+                                    vehicle.VehMessage = "Car can't be on the road";
+                                    return vehicle;
+                                }
+                                vehicle.VehMessage = "Car can be on the road";
+                                return vehicle;
+                            }
+                            vehicle.VehMessage = "Car can be on the road";
+                            return vehicle;
+                        default:
+                            vehicle.VehMessage = "Invalid Plate Number";
+                            return vehicle;
+                    }
+
+                }
+                vehicle.VehMessage = "Invalid Date Format";
+                return vehicle;
+            }
+            vehicle.VehMessage = "Invalid Time";
+            return vehicle;
+        }
+                    
+    }
+    
+}
---
+++ MethodsTests/ControlMethodsTests.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Methods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicle;
+
+namespace Methods.Tests
+{
+    [TestClass()]
+    public class ControlMethodsTests
+    {
+        /// <summary>
+        ///Test of writing a date with '-'
+        /// </summary>
+        [TestMethod()]
+        public void VehicleOnRoadDateFormatTest()
+        {
+            var TestVehicle = new VehicleObject();
+            TestVehicle.VehLicensePlateNumber = "PBO-0859";
+            TestVehicle.VehMessage = "Car can be on the road";
+            var vehicle = new VehicleObject();
+            vehicle.VehLicensePlateNumber ="PBO-0859";
+            ControlMethods methods = new ControlMethods();
+            TimeSpan hourOfTheDay = new TimeSpan(19,30,0);            
+            Assert.AreEqual(TestVehicle.VehMessage, methods.VehicleOnRoad(vehicle, "14-04-2017", hourOfTheDay).VehMessage);
+        }
+        /// <summary>
+        /// Test of time passed the period of 'Pico y Placa'
+        /// </summary>
+        [TestMethod()]
+        public void VehicleOnRoadTimeTest()
+        {
+            var TestVehicle = new VehicleObject();
+            TestVehicle.VehLicensePlateNumber = "PBO-0859";
+            TestVehicle.VehMessage = "Car can be on the road";
+            var vehicle = new VehicleObject();
+            vehicle.VehLicensePlateNumber = "PBO-0859";
+            ControlMethods methods = new ControlMethods();
+            TimeSpan hourOfTheDay = new TimeSpan(19,30,0);
+            Assert.AreEqual(TestVehicle.VehMessage, methods.VehicleOnRoad(vehicle, "14-04-2017", hourOfTheDay).VehMessage);
+        }
+        /// <summary>
+        /// Test of writing a date with '/'
+        /// </summary>
+        [TestMethod()]
+        public void VehicleOnRoadDateFormatTest2()
+        {
+            var TestVehicle = new VehicleObject();
+            TestVehicle.VehLicensePlateNumber = "PBO-0859";
+            TestVehicle.VehMessage = "Car can be on the road";
+            var vehicle = new VehicleObject();
+            vehicle.VehLicensePlateNumber = "PBO-0859";
+            ControlMethods methods = new ControlMethods();
+            TimeSpan hourOfTheDay = new TimeSpan(21,0,0);
+            Assert.AreEqual(TestVehicle.VehMessage, methods.VehicleOnRoad(vehicle, "14-04-2017", hourOfTheDay).VehMessage);
+        }
+        /// <summary>
+        /// Test of time inside the period of 'Pico y Placa'
+        /// </summary>
+        [TestMethod()]
+        public void VehicleOnRoadTimeTest2()
+        {
+            var TestVehicle = new VehicleObject();
+            TestVehicle.VehLicensePlateNumber = "PBO-0859";
+            TestVehicle.VehMessage = "Car can't be on the road";
+            var vehicle = new VehicleObject();
+            vehicle.VehLicensePlateNumber = "PBO-0859";
+            ControlMethods methods = new ControlMethods();
+            TimeSpan hourOfTheDay = new TimeSpan(18, 29, 0);
+            Assert.AreEqual(TestVehicle.VehMessage, methods.VehicleOnRoad(vehicle, "14-04-2017", hourOfTheDay).VehMessage);
+        }
+        /// <summary>
+        /// Test of invalid Date Format
+        /// </summary>
+        [TestMethod()]
+        public void VehicleOnRoadInvalidDateFormatTest()
+        {
+            var TestVehicle = new VehicleObject();
+            TestVehicle.VehLicensePlateNumber = "PBO-0859";
+            TestVehicle.VehMessage = "Invalid Date Format";
+            var vehicle = new VehicleObject();
+            vehicle.VehLicensePlateNumber = "PBO-0859";
+            ControlMethods methods = new ControlMethods();
+            TimeSpan hourOfTheDay = new TimeSpan(18, 29, 0);
+            Assert.AreEqual(TestVehicle.VehMessage, methods.VehicleOnRoad(vehicle, "2017-04-04", hourOfTheDay).VehMessage);            
+        }
+        /// <summary>
+        /// Test of Invalid Time
+        /// </summary>
+        [TestMethod()]
+        public void VehicleOnRoadInvalidTimeTest()
+        {
+            var TestVehicle = new VehicleObject();
+            TestVehicle.VehLicensePlateNumber = "PBO-0859";
+            TestVehicle.VehMessage = "Invalid Time";
+            var vehicle = new VehicleObject();
+            vehicle.VehLicensePlateNumber = "PBO-0859";
+            ControlMethods methods = new ControlMethods();
+            TimeSpan hourOfTheDay = new TimeSpan(36,0,0);
+            Assert.AreEqual(TestVehicle.VehMessage, methods.VehicleOnRoad(vehicle, "14-04-2017", hourOfTheDay).VehMessage);
+        }
+    }
+}
